41770a224786c3594c5e653a763d7c3fcb5265cf
Mikarika <nast.larina2013@yandex.ru>
2021-09-17 20:35:16 +0300

    Remove unused constructor

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -3,11 +3,6 @@ public class Node {
     Object value;
     Node next;
 
-    Node(Object v, Node n) {
-        this.value = v;
-        this.next = n;
-    }
-
     public Node(Object value) {
         this.value = value;
     }
